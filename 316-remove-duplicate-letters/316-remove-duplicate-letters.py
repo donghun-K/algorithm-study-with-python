d5a04e2e995dc60e2e530e36495f1090f1222dbc
DongHun, Kim <60064471+donghun-K@users.noreply.github.com>
2022-07-12 01:27:31 +0900

    Time: 62 ms (38.74%), Space: 13.9 MB (40.41%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.py
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.py
@@ -1,8 +1,16 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
-        for char in sorted(set(s)):
-            suffix = s[s.index(char):]
+        counter = collections.Counter(s)
+        seen = set()
+        stack = []
+        
+        for char in s:
+            counter[char] -= 1
+            if char in seen:
+                continue
+            while stack and char < stack[-1] and counter[stack[-1]] > 0:
+                seen.remove(stack.pop())
+            stack.append(char)
+            seen.add(char)
             
-            if set(s) == set(suffix):
-                return char + self.removeDuplicateLetters(suffix.replace(char, ''))
-        return ''
+        return ''.join(stack)
