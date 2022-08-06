c1ff06b430de64e6a114a8029bab87a8a66e5243
DongHun, Kim <60064471+donghun-K@users.noreply.github.com>
2022-08-06 21:27:48 +0900

    Time: 307 ms (29.11%), Space: 15.9 MB (39.39%) - LeetHub

--- 455-assign-cookies/455-assign-cookies.py
+++ 455-assign-cookies/455-assign-cookies.py
@@ -3,11 +3,11 @@ class Solution:
         g.sort()
         s.sort()
         
-        child_i = cookie_j = 0
+        result = 0
         
-        while child_i < len(g) and cookie_j < len(s):
-            if s[cookie_j] >= g[child_i]:
-                child_i += 1
-            cookie_j += 1
-        
-        return child_i
+        for i in s:
+            index = bisect.bisect_right(g, i)
+            if index > result:
+                result += 1
+                
+        return result
