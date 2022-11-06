e627f9da6f7c45c5683500ae20df27309a9942ba
DongHun, Kim <60064471+donghun-K@users.noreply.github.com>
2022-11-06 15:00:42 +0900

    Update 수 정렬하기.py

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,9 +1,23 @@
 import sys
 
-n = int(sys.stdin.readline())
-arr = []
-for i in range(n):
-    arr.append(int(sys.stdin.readline()))
-    
-for e in sorted(arr):
-    print(e)
+input = sys.stdin.readline
+
+# Bubble sort
+
+N = int(input())
+A = [0] * N
+
+for i in range(N):
+    A[i] = int(input())
+
+for i in range(N - 1):
+    count = 0
+    for j in range(N - 1 - i):
+        if A[j] > A[j + 1]:
+            A[j], A[j + 1] = A[j + 1], A[j]
+            count += 1
+    if count == 0:
+        break
+
+for num in A:
+    print(num)
