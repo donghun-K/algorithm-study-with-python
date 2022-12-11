6247c1d2409f3b47a2dfb23d385925eff4297bb9
DongHun, Kim <60064471+donghun-K@users.noreply.github.com>
2022-12-11 18:09:25 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.06 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.py
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -1,2 +1,3 @@
-def solution(n):
-    return len([i for i in range(1, n + 1) if n % i == 0])
+import math
+def solution(n):   
+    return len([i for i in range(1, int(math.sqrt(n)) +1) if n % i == 0 ]) * 2 - (math.sqrt(n) == math.sqrt(n)//1)
