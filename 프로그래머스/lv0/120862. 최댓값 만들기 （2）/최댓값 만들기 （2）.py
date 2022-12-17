60c8d87a9678968cfb8807ade915516e27558fa4
DongHun, Kim <60064471+donghun-K@users.noreply.github.com>
2022-12-17 16:49:19 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,28 +1,3 @@
 def solution(numbers):
-    pos = []
-    neg = []
-    zero = False
-    for num in numbers:
-        if num > 0:
-            pos.append(num)
-        elif num < 0:
-            neg.append(num)
-        else:
-            zero = True
-
-    pos.sort(reverse=True)
-    neg.sort()
-
-    if len(pos) >= 2 and len(neg) >= 2:
-        return max(pos[0] * pos[1], neg[0] * neg[1])
-
-    if len(pos) >= 2:
-        return pos[0] * pos[1]
-
-    if len(neg) >= 2:
-        return neg[0] * neg[1]
-
-    if zero:
-        return 0
-
-    return neg[-1] * pos[-1]
+    numbers = sorted(numbers)
+    return max(numbers[0] * numbers[1], numbers[-1] * numbers[-2])
