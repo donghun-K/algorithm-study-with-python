28a3074bf4bb519819ab83baed21d0ddff5a68ed
DongHun, Kim <60064471+donghun-K@users.noreply.github.com>
2022-12-30 11:36:48 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 64.34 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,7 +1,7 @@
-def solution(participant, completion):    
-    participant.sort()
-    completion.sort()
-    for i in range(len(completion)):
-        if participant[i] != completion[i]:
-            return participant[i]
-    return participant[-1]
+import collections
+
+
+def solution(participant, completion):
+    return list(
+        (collections.Counter(participant) - collections.Counter(completion)).keys()
+    )[0]
