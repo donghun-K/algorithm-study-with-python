f41456190368fc947264b67a9c6f8a7cb821f8e5
DongHun, Kim <60064471+donghun-K@users.noreply.github.com>
2023-04-04 15:28:18 +0900

    [Unrated] Title: 단어의 개수, Time: 180 ms, Memory: 22668 KB -BaekjoonHub

---
+++ 백준/Unrated/1152. 단어의 개수/단어의 개수.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let arr = input[0].trim().split(' ');
+
+if (arr == '') console.log(0);
+else console.log(arr.length);
