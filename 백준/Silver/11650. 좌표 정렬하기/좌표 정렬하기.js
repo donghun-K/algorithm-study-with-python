85214e626030c7da96c26c8abde21035f4d97165
DongHun, Kim <60064471+donghun-k@users.noreply.github.com>
2023-06-20 19:58:06 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 508 ms, Memory: 58228 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const n = Number(input[0]);
+
+const points = [];
+
+for (let i = 1; i <= n; i++) {
+  const [x, y] = input[i].split(' ').map(Number);
+  points.push([x, y]);
+}
+
+points.sort((a, b) => {
+  if (a[0] === b[0]) return a[1] - b[1];
+  return a[0] - b[0];
+});
+
+let answer = '';
+
+points.forEach(([x, y]) => {
+  answer += x + ' ' + y + '\n';
+});
+
+console.log(answer);
