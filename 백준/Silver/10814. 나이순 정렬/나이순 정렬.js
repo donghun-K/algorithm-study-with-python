e78cb1acb6d82b4b5cfef9b2f65c68ede1930107
DongHun, Kim <60064471+donghun-k@users.noreply.github.com>
2023-06-25 13:39:26 +0900

    [Silver V] Title: 나이순 정렬, Time: 412 ms, Memory: 49708 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const input = fs.readFileSync('./dev/stdin').toString().split('\n');
+
+const n = Number(input[0]);
+const table = [];
+
+for (let i = 1; i <= n; i++) {
+  const [age, name] = input[i].split(' ');
+  table.push([Number(age), name]);
+}
+
+table.sort((a, b) => {
+  if (a[0] !== b[0]) return a[0] - b[0];
+  return a[1] - b[1];
+});
+
+let answer = '';
+
+for (person of table) {
+  answer += `${person[0]} ${person[1]}\n`;
+}
+
+console.log(answer);
